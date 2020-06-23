8538ab88dbb13b751c0bfd355a94dc98643f487d
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2020-06-23 14:23:32 -0400

    update optimization routine for cached and on-demand parameterization of local curvature

--- bin/optimize_fem.py
+++ bin/optimize_fem.py
@@ -1,50 +1,55 @@
 #!/usr/bin/env python
 '''
-Given a full head model, description of quadratic input domain, and cost weights use Bayesian Optimization from Cornell-MOE to find the optimal input (position and rotation).
+Given a full head model, description of quadratic input domain,
+and cost weights use Bayesian Optimization from Cornell-MOE to find the optimal
+input (position and rotation).
 
 Usage:
-    optimize_fem.py [options] <mesh> <weightfile> <quad_const> <bounds> <affine> <coil> <loc_out> <rot_out>
+    optimize_fem.py [options] <mesh> <weightfile> <init_centroid> <coil> <out_basename>
 
 Arguments:
     <mesh>                                  GMSH mesh file (v2 or v4)
-    <weightfile>                            1D array consisting of 1 value per node
-    <quad_const>                            Quadratic constants for input domain
-    <bounds>                                Search bounds (x,y) for quadratic surface
-    <affine>                                Affine matrix that takes you from canonical orientation
-                                            to surface RAS used in mesh
-    <coil>                                  Coil magnetization field (.nii.gz) from SimNIBS
-    <loc_out>                               Output position vector in RAS
-    <rot_out>                               Output rotation vector to smoothed head surface
+    <weightfile>                            1D array consisting of 1 value per
+                                            node
+    <init_centroid>                         Initial centroid to start sampling
+    <coil>                                  Coil magnetization field (.nii.gz)
+                                            from SimNIBS
+    <out_basename>                          Output basename
 
 Options:
-    -c,--cpus N_CPUS                        Number of CPUS to use to calculate objective func
-                                            [Default: 8]
-    -t,--tmp-dir                            Directory to perform FEM experiments in
+    -c,--cpus N_CPUS                        Number of CPUS to use to calculate
+                                            objective func
+                                            [Default: 1]
+    -t,--tmp-dir TMPDIR                     Directory to perform FEM
+                                            experiments in
                                             [Default: $TMPDIR]
                                             [Fallback: /tmp/]
-    -n, --n-iters ITERS                     Maximum number of iterations to perform
+    -n, --n-iters ITERS                     Maximum number of iterations
+                                            to perform
                                             [Default: 50]
-    -m, --min-var-samps                     Minimum number of samples required to check convergence
+    -m, --min-var-samps                     Minimum number of samples required
+                                            to check convergence
                                             [Default: 10]
-    -c,--convergence TOL                    Convergence threshold for shifts in input space (decimal number)
+    -c,--convergence TOL                    Convergence threshold for shifts
+                                            in input space (decimal number)
                                             [Default: 1e-3]
     -h,--history FILE                       Save best point history
                                             [Default: disabled]
-    -s,--skip-convergence                   Do not use convergence criterion, instead use n-iters only
+    -s,--skip-convergence                   Do not use convergence criterion,
+                                            instead use n-iters only
                                             [Default: disabled]
-    -r,--retries RETRIES                    Number of retries if bad condition number is reached
-                                            [Default: 5]
 '''
 
-#Base package loading
+# Base package loading
+
 import os
 import numpy as np
-from fieldopt import geolib
 from fieldopt.objective import FieldFunc
 from docopt import docopt
 from collections import deque
 
-#Cornell package loading
+# Cornell package loading
+
 from moe.optimal_learning.python.cpp_wrappers.domain import TensorProductDomain as cTensorProductDomain
 from moe.optimal_learning.python.python_version.domain import TensorProductDomain
 from moe.optimal_learning.python.geometry_utils import ClosedInterval
@@ -83,15 +88,12 @@ def main():
 
     args = docopt(__doc__)
 
-    #Parse arguments
+    # Parse arguments
     mesh = args['<mesh>']
     weights = np.load(args['<weightfile>'])
-    C = np.load(args['<quad_const>'])
-    b = np.load(args['<bounds>'])
-    R = np.load(args['<affine>'])
+    init_centroid = np.genfromtxt(args['<init_centroid>'])
     coil = args['<coil>']
-    loc_out = args['<loc_out>']
-    rot_out = args['<rot_out>']
+    out_basename = args['<out_basename>']
     cpus = int(args['--cpus']) or 8
     tmpdir = args['--tmp-dir'] or os.getenv('TMPDIR') or "/tmp/"
     num_iters = int(args['--n-iters']) or 50
@@ -99,61 +101,50 @@ def main():
     tol = float(args['--convergence']) or 0.001
     history = args['--history']
     skip_convergence = args['--skip-convergence']
-    retries = int(args['--retries']) or 5
 
-    #Make search domain
+    print('Using {} cpus'.format(cpus))
+
+    f = FieldFunc(mesh_file=mesh,
+                  initial_centroid=init_centroid,
+                  tet_weights=weights,
+                  coil=coil,
+                  field_dir=tmpdir,
+                  cpus=cpus)
+
+    # Make search domain
     search_domain = TensorProductDomain([
-        ClosedInterval(b[0, 0], b[0, 1]),  #X coord on quadratic surface
-        ClosedInterval(b[1, 0], b[1, 1]),  #Y coord on quadratic surface
-        ClosedInterval(0, 180)  #Rotational angle
-    ])
-
-    c_search_domain = cTensorProductDomain([
-        ClosedInterval(b[0, 0], b[0, 1]),
-        ClosedInterval(b[1, 0], b[1, 1]),
+        ClosedInterval(f.bounds[0, 0], f.bounds[0, 1]),
+        ClosedInterval(f.bounds[1, 0], f.bounds[1, 1]),
         ClosedInterval(0, 180)
     ])
 
-    #Make objective function
-    f = FieldFunc(mesh_file=mesh,
-                  quad_surf_consts=C,
-                  surf_to_mesh_matrix=R,
-                  tet_weights=weights,
-                  field_dir=tmpdir,
-                  coil=coil,
-                  cpus=cpus)
+    c_search_domain = cTensorProductDomain([
+        ClosedInterval(f.bounds[0, 0], f.bounds[0, 1]),
+        ClosedInterval(f.bounds[1, 0], f.bounds[1, 1]),
+        ClosedInterval(0, 180)
+    ])
 
-    #Generate historical points
-
-    #Set up model specifications
+    # Generate historical points
     prior = DefaultPrior(n_dims=3 + 2, num_noise=1)
-    hist_pts = 10
+    hist_pts = cpus
     i = 0
-    while i < retries:
-        init_pts = search_domain.generate_uniform_random_points_in_domain(
-            hist_pts)
-        observations = -f.evaluate(init_pts)
-        hist_data = HistoricalData(dim=3, num_derivatives=0)
-        hist_data.append_sample_points([
-            SamplePoint(inp, o, 0.0) for o, inp in zip(observations, init_pts)
-        ])
-        try:
-            gp_ll = GaussianProcessLogLikelihoodMCMC(historical_data=hist_data,
-                                                     derivatives=[],
-                                                     prior=prior,
-                                                     chain_length=1000,
-                                                     burnin_steps=2000,
-                                                     n_hypers=2**4,
-                                                     noisy=False)
-            gp_ll.train()
-        except ValueError:
-            print('Ran into bad condition with initial samples')
-            print('Regenerating initial samples')
-            i = i + 1
-        else:
-            break
+    init_pts = search_domain.generate_uniform_random_points_in_domain(hist_pts)
+    observations = -f.evaluate(init_pts)
+    hist_data = HistoricalData(dim=3, num_derivatives=0)
+    hist_data.append_sample_points(
+        [SamplePoint(inp, o, 0.0) for o, inp in zip(observations, init_pts)])
 
-    #Initialize grad desc params
+    # Train GP model
+    gp_ll = GaussianProcessLogLikelihoodMCMC(historical_data=hist_data,
+                                             derivatives=[],
+                                             prior=prior,
+                                             chain_length=1000,
+                                             burnin_steps=2000,
+                                             n_hypers=2**4,
+                                             noisy=False)
+    gp_ll.train()
+
+    # Initialize grad desc params
     sgd_params = cGDParams(num_multistarts=200,
                            max_num_steps=50,
                            max_num_restarts=5,
@@ -170,25 +161,25 @@ def main():
     var_buffer = deque(maxlen=min_samps)
     for i in np.arange(0, num_iters):
 
-        #Optimize qEI and pick samples
+        # Optimize qEI and pick samples
         points_to_sample, ei = gen_sample_from_qei(gp_ll.models[0],
                                                    c_search_domain,
                                                    sgd_params=sgd_params,
                                                    num_samples=num_samples,
                                                    num_mc=2**10)
 
-        #Collect observations
+        # Collect observations
         sampled_points = -f.evaluate(points_to_sample)
         evidence = [
             SamplePoint(c, v, 0.0)
             for c, v in zip(points_to_sample, sampled_points)
         ]
 
-        #Update model
+        # Update model
         gp_ll.add_sampled_points(evidence)
         gp_ll.train()
 
-        #Pull model and pull values
+        # Pull model and pull values
         gp = gp_ll.models[0]
         min_point = np.argmin(gp._points_sampled_value)
         min_val = np.min(gp._points_sampled_value)
@@ -205,7 +196,7 @@ def main():
 
         if history:
             with open(history, 'w') as buf:
-                buf.writelines(list(best_point_history))
+                buf.write('\n'.join(best_point_history))
 
         # Convergence check
         if (len(var_buffer) == var_buffer.maxlen) and not skip_convergence:
@@ -216,16 +207,17 @@ def main():
                 print('History length: {}'.format(var_buffer.maxlen))
                 print('Tolerance: {}'.format(tol))
                 break
+
         var_buffer.append(min_val)
 
-    #Once sampling is done take the best point and transform it back into native space
-    preaff_loc = geolib.map_param_2_surf(best_coord[0], best_coord[1], C)
-    preaff_rot, _ = geolib.map_rot_2_surf(best_coord[0], best_coord[1],
-                                          best_coord[2], C)
-    loc = np.matmul(R, preaff_loc)
-    rot = np.matmul(R, preaff_rot)
-    np.savetxt(loc_out, loc)
-    np.savetxt(rot_out, rot)
+    # Run final simulation
+    out_sim = f"{out_basename}_optimal_sim.msh"
+    out_coilpos = f"{out_basename}_optimal_coilpos.geo"
+    _, matsimnibs = f.run_simulation(best_coord, out_sim, out_coilpos)
+
+    # Save position and orientation matrix
+    np.savetxt(f"{out_basename}_orientation.txt", matsimnibs)
+
 
 if __name__ == '__main__':
     main()
