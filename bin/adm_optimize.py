5b25a88a552e365ea27016d3d6b3190bb667b894
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-08-03 16:02:48 -0400

    First draft of ADM optimization script

---
+++ bin/adm_optimize.py
@@ -0,0 +1,54 @@
+import argparse
+import numpy as np
+from simnibs import opt_struct
+
+
+def main():
+    p = argparse.ArgumentParser(description="Run ADM optimization")
+    p.add_argument("msh",
+                   help="Subject realistic head model .msh file",
+                   type=str)
+    p.add_argument("coordinates",
+                   help="Text file of RAS coordinates for target",
+                   type=str)
+    p.add_argument("distance",
+                   help="Distance from scalp to use in optimization",
+                   type=float)
+    p.add_argument("coil", help=".ccd coil definition file", type=str)
+    p.add_argument("--radius", help="Radius of ROI", type=float)
+    p.add_argument("--sim_result", help="Simulation .msh output", type=str)
+    p.add_argument("--sim_geo", help="Simulation .geo output", type=str)
+    p.add_argument("--ncpus",
+                   help="Number of CPUS to use in optimization",
+                   type=int,
+                   default=4)
+    p.add_argument("orientation",
+                   help="Optimal coil orientation matrix output")
+
+    subparsers = p.add_subparsers(help="Mode of optimization")
+
+    radial_parser = subparsers.add_parser(
+        "radial", help="Use E-Field directional optimization")
+    radial_parser.add_argument(
+        "direction",
+        help="Vector text file containing E-Field direction",
+        type=str)
+
+    mag_parser = subparsers.add_parser(
+        "magnitude", help="Use E-Field magnitude optimization")
+    mag_parser.set_defaults(direction=None)
+
+    args = p.parse_args()
+
+    tms_opt = opt_struct.TMSoptimize()
+    tms_opt.fnamehead = args.msh
+    tms_opt.fnamecoil = args.coil
+    tms_opt.target = np.load(args.coordinates)
+    if args.direction is not None:
+        tms_opt.target_direction = np.load(args.direction)
+    tms_opt.distance = args.distance
+    tms_opt.target_size = args.radius
+    tms_opt.method = "ADM"
+    tms_opt.run(cpus=args.ncpus)
+
+    # TODO: Run this and rename outputs sensibly
